7d5809b466bfc5c685d73ede8403deb71d9212f6
birknergard <birknergard@gmail.com>
2025-05-05 07:55:45 -0400

    Added file type checking (has to be .txt file) to input file.

--- pg3401_exam25/task4_87/task4_threads.c
+++ pg3401_exam25/task4_87/task4_threads.c
@@ -4,6 +4,7 @@
  * My solution to task 4 part I and II 
  * Since it says in the exam description that I am not to create any files,
  * ive put everything I would have put in a header file in this file.
+ *
  * */
 
 /* Part 1 Output
@@ -36,6 +37,10 @@ typedef struct _THREAD_ARGS {
    BYTE byarrBuffer[BUFFER_SIZE];
    int iBytesInBuffer; 
 
+   /* Flags */
+   int iReaderFailed;
+   int iCounterFailed;
+
    /* Filename for reader thread */
    char *szFileName;
 } THREAD_ARGS;
@@ -45,30 +50,33 @@ typedef struct _THREAD_ARGS {
  * */
 int DBJ2_Hash(FILE * fFileDescriptor, int* piHash);
 void TEA_Encrypt(unsigned int *const v,unsigned int *const w, const unsigned int *const k);
+int isTxtFile(char *szFileName);
 
 /* Renamed from A to Reader thread (since its main job reading from file to the buffer) */
 void* ReaderThread(void* vpArgs) {
    /* Declare variables */
-   FILE *fp = NULL;
    int iTotalBytesRead = 0;
    int iFileSize = 0;
    int iReadByteFromFile = 0;
+
+   /* Declare file pointer */
+   FILE *fpSource = NULL;
 
    /* Typecast pointer to arg struct (THEAD_ARGS) 
     * and store a reference for each field locally for more clean code */
    THREAD_ARGS *tData = (THREAD_ARGS *) vpArgs; 
 
-   /* Opens file for reading */
-   fp = fopen(tData->szFileName, "r");
-   if (fp == NULL) {
+   /* Open source file */
+   fpSource = fopen(tData->szFileName, "r");
+   if (fpSource == NULL) {
       perror("Failed to open file");
       exit(EXIT_FAILURE);
    }
 
    /* Retrieve the size of file */
-   if(fseek(fp, 0, SEEK_END) == 0){
-      iFileSize = ftell(fp);
-      rewind(fp);
+   if(fseek(fpSource, 0, SEEK_END) == 0){
+      iFileSize = ftell(fpSource);
+      rewind(fpSource);
    }
 
    printf("READER: Thread started! File is of size %d \n", iFileSize);
@@ -77,7 +85,7 @@ void* ReaderThread(void* vpArgs) {
    while (1) {
 
       /* Reads one byte at a time from the file into the byte array */
-      iReadByteFromFile = fread((tData->byarrBuffer + tData->iBytesInBuffer), 1, 1, fp);
+      iReadByteFromFile = fread((tData->byarrBuffer + tData->iBytesInBuffer), 1, 1, fpSource);
       if(iReadByteFromFile == 0 && iTotalBytesRead != iFileSize){
          /* If whole file wasn't read, we print and error first */
          perror("Read of file failed!");
@@ -135,8 +143,8 @@ void* ReaderThread(void* vpArgs) {
    }
 
    /* Close file handle and set file pointer to NULL */
-   fclose(fp);
-   fp = NULL;
+   fclose(fpSource);
+   fpSource = NULL;
 
    /* Exits thread */
    pthread_exit(NULL);
@@ -145,63 +153,100 @@ void* ReaderThread(void* vpArgs) {
 /* Renamed from B to Counter (since its main job is counting each byte in the buffer) */
 void* CounterThread(void* vpArgs) {
    /* Declaring variables */
-   int i, iReaderComplete = 0;
+   int i, iReaderComplete = 0, iSetupFailed = 0;
 
    /* Casting void * to argument struct * */
    THREAD_ARGS *tData = (THREAD_ARGS *) vpArgs; 
+
+   /* Declaring file pointers */
+   FILE *fpHashed = NULL;
+   FILE *fpEncrypted = NULL;
+
+   /* Opening/creating files, sets flag to failure if failed to prevent thread from running */
+   fpHashed = fopen("./task4_pg2265.hash", "wb");
+   if(fpHashed == NULL){
+      perror("Failed to open .hash file.");
+      iSetupFailed = 1;
+   }
+   
+   fpEncrypted = fopen("./task4_pg2265.enc", "wb");
+   if(fpEncrypted == NULL){
+      perror("Failed to open .enc file.");
+
+      /* Set failure flag to true and signal reader */
+      tData->iCounterFailed = 1;
+      sem_post(&tData->semCounterDone);
+   }
 
    /* Initializing counter array before counter loop starts */ 
    memset(tData->iarrByteCount, 0, sizeof(int) * BYTE_RANGE);
 
    printf("COUNTER: Thread started!\n");
-   /* Runs counter infinitely, or until break condition */
-   while (1) {
+   /* Checks whether setup succeeded. Counter only runs if true */
+   if(iSetupFailed == 0){
+      /* Runs counter infinitely, or until break condition */
+      while (1) {
 
-      /* Waits for available semaphore from reader */
-      printf("COUNTER: Waiting for signal from reader ...\n");
-      sem_wait(&tData->semReaderDone);
+         /* Waits for available semaphore from reader */
+         printf("COUNTER: Waiting for signal from reader ...\n");
+         sem_wait(&tData->semReaderDone);
 
-      /* Locks mutex while counter is running to prevent unexpected changes */
-      pthread_mutex_lock(&tData->muMutex);
-      printf("COUNTER: Signal received! Starting counter...\n");
+         /* Locks mutex while counter is running to prevent unexpected changes */
+         pthread_mutex_lock(&tData->muMutex);
+         printf("COUNTER: Signal received! Starting counter...\n");
 
-      /* If bytes in buffer is less than max, that means the reader didnt have more
-       * data to read. therefore we can break the loop when this happens. */
-      if(tData->iBytesInBuffer < BUFFER_SIZE){
-         printf("COUNTER: Reader is done reading the file! Completing count ...\n");
-         iReaderComplete = 1;
+         /* If bytes in buffer is less than max, that means the reader didnt have more
+          * data to read. therefore we can break the loop when this happens. */
+         if(tData->iBytesInBuffer < BUFFER_SIZE){
+            printf("COUNTER: Reader is done reading the file! Completing count ...\n");
+            iReaderComplete = 1;
+         }
+
+         /* Counts the number of a byte's occurence in the buffer */
+         /* TODO: Task 2: Replace this with hash and encryption methods */
+         for(i = 0; i < tData->iBytesInBuffer; i++){
+            /* Indexes by the BYTE (value between 0 and MAX_RANGE) and increments that value */
+            tData->iarrByteCount[tData->byarrBuffer[i]]++;
+         }
+
+         /* TODO: DJB2 HASH */
+         
+
+         /* TODO: TEA ENCRYPT */
+
+
+         printf("Bytes added to list!\n");
+         /* Resets the number of bytes in buffer */
+         tData->iBytesInBuffer = 0;
+
+         /* If bytes were less than max when signal was received, we exit the program here. */
+         if(iReaderComplete == 1){
+            pthread_mutex_unlock(&tData->muMutex);
+            break;
+         } else {
+            /* Otherwise we signal back to the reader to continue */
+            printf("COUNTER: Buffer is empty! Signaling reader to start.\n");
+            pthread_mutex_unlock(&tData->muMutex);
+            sem_post(&tData->semCounterDone);
+         }
       }
-
-      /* Counts the number of a byte's occurence in the buffer */
-      /* TODO: Task 2: Replace this with hash and encryption methods */
-      for(i = 0; i < tData->iBytesInBuffer; i++){
-         /* Indexes by the BYTE (value between 0 and MAX_RANGE) and increments that value */
-         tData->iarrByteCount[tData->byarrBuffer[i]]++;
-      }
-
-      printf("Bytes added to list!\n");
-      /* Resets the number of bytes in buffer */
-      tData->iBytesInBuffer = 0;
-
-
-      /* If bytes were less than max when signal was received, we exit the program here. */
-      if(iReaderComplete == 1){
-         pthread_mutex_unlock(&tData->muMutex);
-         break;
-      } else {
-         printf("COUNTER: Buffer is empty! Signaling reader to start.\n");
-         pthread_mutex_unlock(&tData->muMutex);
-         /* Otherwise we signal back to the reader to continue */
-         sem_post(&tData->semCounterDone);
+      /* Prints counter results to the terminal */
+      for(i = 0; i < BYTE_RANGE; i++){
+         printf("%c: %d - ", i, tData->iarrByteCount[i]);
+         if(i % 5 == 0) puts("");
       }
    }
 
-   /* Prints counter results to the terminal */
-   for(i = 0; i < BYTE_RANGE; i++){
-      printf("%c: %d - ", i, tData->iarrByteCount[i]);
-      if(i % 5 == 0) puts("");
+   /* Closing files (if they are open) */
+   if(fpHashed != NULL){
+      fclose(fpHashed);
+      fpHashed = NULL;
    }
 
+   if(fpEncrypted != NULL){
+      fclose(fpEncrypted);
+      fpEncrypted = NULL;
+   }
 
    /* Closes thread */
    pthread_exit(NULL);
@@ -211,7 +256,7 @@ int main(int iArgC, char **arrpszArgV) {
    /* Declaring variables */
    pthread_t thrReader, thrCounter;
    THREAD_ARGS *tData = NULL; /* (t)hread Data */
-   
+
    printf("Preparing program.\n");
    /* Next two statements are there to verify argv. Whether it exists at all ... */
    if(arrpszArgV[1] == NULL){
@@ -225,6 +270,15 @@ int main(int iArgC, char **arrpszArgV) {
       exit(1);
    }
 
+   if(isTxtFile(arrpszArgV[1]) == 0){
+      perror("Given file is not a .txt file.");
+      exit(1);
+   }
+
+   /* Since source file is passed from user, ensure user can only open .txt files
+      If user attempts to open .enc or .hash file it could cause major issues, so we just restrict
+      user to .txt files */
+
    /* Allocating thread data */
    tData = (THREAD_ARGS *) malloc(sizeof(THREAD_ARGS));
    if(tData == NULL){
@@ -232,12 +286,15 @@ int main(int iArgC, char **arrpszArgV) {
       exit(1);
    }
 
-
    /* Passes the given filename into thread data struct */
    tData->szFileName = arrpszArgV[1];
 
    /* Initialize data */
    tData->iBytesInBuffer = 0;
+   
+   /* Set initial boolean flags */
+   tData->iReaderFailed = 0;
+   tData->iCounterFailed = 0;
 
    /* Initialize mutex (with default attributes) */
    pthread_mutex_init(&tData->muMutex, NULL);
@@ -286,6 +343,32 @@ int main(int iArgC, char **arrpszArgV) {
    free(tData);
 
    return 0;
+}
+
+/*
+ * Checks if filename given is of the .txt type
+ * */
+int isTxtFile(char *szFileName){
+   int iLength = strlen(szFileName);
+   int i, j;
+   /* One extra byte is added to account for '\0' */
+   char szSubstring[5];
+
+   /* Initialize string */
+   memset(szSubstring, 0, 5);
+
+   /* Get last four letters of string */
+   j = 0;
+   for(i = iLength - 4; i < iLength; i++){
+      szSubstring[j] = szFileName[i]; 
+      j++;
+   }
+   szSubstring[4] = '\0';
+   
+   /* Check if substring == ".txt" */
+   if(strncmp(szSubstring, ".txt", 4) == 0){
+      return 1;
+   } else return 0;
 }
 
 
