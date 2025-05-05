53c3e2e1ec02196c9ac105808a5b17f99cdb9077
birknergard <birknergard@gmail.com>
2025-05-05 12:58:27 -0400

    Counter now opens hash and enc files, and threads verify each other before running.

--- pg3401_exam25/task4_87/task4_threads.c
+++ pg3401_exam25/task4_87/task4_threads.c
@@ -28,21 +28,21 @@
 
 typedef struct _THREAD_ARGS {
    /* Synchronization controllers */
-   pthread_mutex_t muMutex;
+   pthread_mutex_t muLock;
    sem_t semReaderDone;
    sem_t semCounterDone;
 
    /* Shared data */
-   int iarrByteCount[BYTE_RANGE];
    BYTE byarrBuffer[BUFFER_SIZE];
    int iBytesInBuffer; 
 
-   /* Flags */
-   int iReaderFailed;
-   int iCounterFailed;
+   /* For initial handshake, only to be edited by their respective threads. 1 if TRUE, 0 if FALSE */ 
+   int iReaderSetupFailed;
+   int iCounterSetupFailed;
 
    /* Filename for reader thread */
    char *szFileName;
+
 } THREAD_ARGS;
 
 /*
@@ -66,85 +66,109 @@ void* ReaderThread(void* vpArgs) {
     * and store a reference for each field locally for more clean code */
    THREAD_ARGS *tData = (THREAD_ARGS *) vpArgs; 
 
-   /* Open source file */
+   /* Attempts to open source file (lock briefly lock mutex because we are accessing filename) */
+   printf("READER: Attempting to open source file ...\n");
+   pthread_mutex_lock(&tData->muLock);
    fpSource = fopen(tData->szFileName, "r");
    if (fpSource == NULL) {
       perror("Failed to open file");
-      exit(EXIT_FAILURE);
-   }
 
-   /* Retrieve the size of file */
-   if(fseek(fpSource, 0, SEEK_END) == 0){
-      iFileSize = ftell(fpSource);
-      rewind(fpSource);
-   }
-
-   printf("READER: Thread started! File is of size %d \n", iFileSize);
-
-   /* Runs thread execution until break condition */
-   while (1) {
-
-      /* Reads one byte at a time from the file into the byte array */
-      iReadByteFromFile = fread((tData->byarrBuffer + tData->iBytesInBuffer), 1, 1, fpSource);
-      if(iReadByteFromFile == 0 && iTotalBytesRead != iFileSize){
-         /* If whole file wasn't read, we print and error first */
-         perror("Read of file failed!");
-         pthread_mutex_unlock(&tData->muMutex);
-
-         /* Signaling other thread
-          * NOTE: Since terminated early, buffer count will the less than max, terminating the counter thread */
-         sem_post(&tData->semReaderDone);
-         break;
-
-      /* Alternatively, if no errors occured, add a byte to the local tracker */
-      } else iTotalBytesRead += 1;
-
-      /* Increments bytes shared bytes read counter by the bytes read during this loop */
-      tData->iBytesInBuffer++;
-
-      /* Break condition, should break when bytes read is equal to file size!! */
-      if(iTotalBytesRead == iFileSize) {
-
-         /* When the whole file has been read, signal counter to count remaining data */
-         printf("READER: No more bytes to read! Signaling counter to read remaining bytes.\n");
-         pthread_mutex_unlock(&tData->muMutex);
-
-         /* Signal counter to continue */
-         sem_post(&tData->semReaderDone);
-
-         /* And exit the loop */
-         break;
+      /* Store outcome of setup in shared memory */
+      tData->iReaderSetupFailed = 1;
+   } else {
+      /* Retrieve the size of file */
+      if(fseek(fpSource, 0, SEEK_END) == 0){
+         iFileSize = ftell(fpSource);
+         rewind(fpSource);
       }
-
-      /* If bytes read have hit the max buffer, signal the counter to start */
-      if(tData->iBytesInBuffer == BUFFER_SIZE){
-
-         printf("READER: Buffer is full! Signaling counter to start.\n");
-
-         /* Unlocks the mutex and signals other thread to continue */
-         pthread_mutex_unlock(&tData->muMutex);
-
-         /* Increment the semaphore */
-         sem_post(&tData->semReaderDone);
-
-         /* Wait for available semaphore */
-         printf("READER: Waiting for signal from counter...\n");
-
-         /* Signals counter thread to start */
-         sem_wait(&tData->semCounterDone);
-
-         /* Locks the mutex to prevent data from being changed while running */
-         pthread_mutex_lock(&tData->muMutex);
-
-         /* Since data from previos loop has been read, we empty the memory buffer */
-         memset(tData->byarrBuffer, 0, BUFFER_SIZE);
-         printf("READER: Signal received! Continuing reader ...\n");
-      }
+      printf("READER: File opened successfully (%d bytes), signaling counter ...\n", iFileSize);
    }
 
-   /* Close file handle and set file pointer to NULL */
-   fclose(fpSource);
-   fpSource = NULL;
+   /* Signal counter to read setup outcome */
+   sem_post(&tData->semReaderDone);
+
+   if(tData->iReaderSetupFailed != 1){
+      /* Waits for counter to signal that it succeeded in opening */
+      pthread_mutex_unlock(&tData->muLock);
+      sem_wait(&tData->semCounterDone);
+      printf("READER: Signal received from counter, checking if succesful ...\n");
+
+      pthread_mutex_lock(&tData->muLock);
+
+      /* If counter didn't fail, start reader */
+      if(tData->iCounterSetupFailed != 1) {
+         printf("READER: Counter was successful! Starting main reader process.");
+
+         /* Runs thread execution until break condition */
+         while (1) {
+
+            /* Reads one byte at a time from the file into the byte array TODO: Make it read more bytes at a time,
+             * this is ineffiecient */
+            iReadByteFromFile = fread((tData->byarrBuffer + tData->iBytesInBuffer), 1, 1, fpSource);
+
+            if(iReadByteFromFile == 0 && iTotalBytesRead != iFileSize){
+               /* If whole file wasn't read, we print an error first */
+               perror("Read of file failed!");
+
+               /* Signaling other thread
+                * NOTE: Since terminated early, buffer count will the less than max, terminating the counter thread */
+               sem_post(&tData->semReaderDone);
+               pthread_mutex_unlock(&tData->muLock);
+               break;
+
+            /* Alternatively, if no errors occured, add a byte to the local tracker */
+            } else iTotalBytesRead += 1;
+
+            /* Increments bytes shared bytes read counter by the bytes read during this loop */
+            tData->iBytesInBuffer++;
+
+            /* Break condition, should break when bytes read is equal to file size!! */
+            if(iTotalBytesRead == iFileSize) {
+
+               /* When the whole file has been read, signal counter to count remaining data */
+               printf("READER: No more bytes to read! Signaling counter to read remaining bytes.\n");
+
+               /* Signal counter to continue */
+               sem_post(&tData->semReaderDone);
+               pthread_mutex_unlock(&tData->muLock);
+
+               /* And exit the loop */
+               break;
+            }
+
+            /* If bytes read have hit the max buffer, signal the counter to start */
+            if(tData->iBytesInBuffer == BUFFER_SIZE){
+
+               printf("READER: Buffer is full! Signaling counter to start.\n");
+
+               /* Increment the semaphore */
+               sem_post(&tData->semReaderDone);
+               pthread_mutex_unlock(&tData->muLock);
+
+               /* Wait for available semaphore */
+               printf("READER: Waiting for signal from counter...\n");
+
+               /* Signals counter thread to start */
+               sem_wait(&tData->semCounterDone);
+
+               /* Locks the mutex to prevent data from being changed while running */
+               pthread_mutex_lock(&tData->muLock);
+
+               /* Since data from previos loop has been read, we empty the memory buffer */
+               memset(tData->byarrBuffer, 0, BUFFER_SIZE);
+               printf("READER: Signal received! Continuing reader ...\n");
+            }
+         } /*-> READER ENDLOOP*/
+      } else {
+         printf("READER: Counter setup failed! Exiting ...");
+      } /*-> COUNTER SETUP SUCCESFUL ENDIF*/
+      pthread_mutex_unlock(&tData->muLock);
+
+      /* Close file handle and set file pointer to NULL */
+      fclose(fpSource);
+      fpSource = NULL;
+
+   } /*-> SOURCE FILE OPENED SUCCESSFULLY ENDIF*/
 
    /* Exits thread */
    pthread_exit(NULL);
@@ -153,7 +177,8 @@ void* ReaderThread(void* vpArgs) {
 /* Renamed from B to Counter (since its main job is counting each byte in the buffer) */
 void* CounterThread(void* vpArgs) {
    /* Declaring variables */
-   int i, iReaderComplete = 0, iSetupFailed = 0;
+   int i, iReaderComplete = 0;
+   int iarrByteCount[BYTE_RANGE];
 
    /* Casting void * to argument struct * */
    THREAD_ARGS *tData = (THREAD_ARGS *) vpArgs; 
@@ -162,91 +187,109 @@ void* CounterThread(void* vpArgs) {
    FILE *fpHashed = NULL;
    FILE *fpEncrypted = NULL;
 
-   /* Opening/creating files, sets flag to failure if failed to prevent thread from running */
-   fpHashed = fopen("./task4_pg2265.hash", "wb");
-   if(fpHashed == NULL){
-      perror("Failed to open .hash file.");
-      iSetupFailed = 1;
-   }
-   
-   fpEncrypted = fopen("./task4_pg2265.enc", "wb");
-   if(fpEncrypted == NULL){
-      perror("Failed to open .enc file.");
 
-      /* Set failure flag to true and signal reader */
-      tData->iCounterFailed = 1;
+   printf("COUNTER: Waiting for signal from reader ...\n");
+   /* Wait for signal from reader */
+   sem_wait(&tData->semReaderDone);
+   pthread_mutex_lock(&tData->muLock);
+
+   /* Only run if neither reader nor counter failed setup */
+   if(tData->iReaderSetupFailed != 1){
+      printf("COUNTER: Signal received! Reader setup was successful! Opening hash and encrypt files...\n");
+
+      /* Opening/creating files, sets flag to failure if failed to prevent reader thread from running */
+      fpHashed = fopen("./task4_pg2265.hash", "wb");
+      if(fpHashed == NULL){
+         perror("Failed to open .hash file.");
+
+         /* Set failure flag to true */
+         tData->iCounterSetupFailed = 1;
+      }
+      
+      fpEncrypted = fopen("./task4_pg2265.enc", "wb");
+      if(fpEncrypted == NULL){
+         perror("Failed to open .enc file.");
+
+         /* Set failure flag to true */
+         tData->iCounterSetupFailed = 1;
+      }
+
+      printf("COUNTER: Attempted open of .hash and .enc files! Signaling reader thread with result ...\n");
+
+      pthread_mutex_unlock(&tData->muLock);
+      /* Signal reader to start operations */
       sem_post(&tData->semCounterDone);
-   }
 
-   /* Initializing counter array before counter loop starts */ 
-   memset(tData->iarrByteCount, 0, sizeof(int) * BYTE_RANGE);
+      if(tData->iCounterSetupFailed != 1){
 
-   printf("COUNTER: Thread started!\n");
-   /* Checks whether setup succeeded. Counter only runs if true */
-   if(iSetupFailed == 0){
-      /* Runs counter infinitely, or until break condition */
-      while (1) {
+         /* Initializing counter array before counter loop starts */ 
+         memset(iarrByteCount, 0, sizeof(int) * BYTE_RANGE);
 
-         /* Waits for available semaphore from reader */
-         printf("COUNTER: Waiting for signal from reader ...\n");
-         sem_wait(&tData->semReaderDone);
+         /* Runs counter infinitely, or until break condition */
+         while(1){
 
-         /* Locks mutex while counter is running to prevent unexpected changes */
-         pthread_mutex_lock(&tData->muMutex);
-         printf("COUNTER: Signal received! Starting counter...\n");
+            /* Waits for available semaphore from reader */
+            sem_wait(&tData->semReaderDone);
 
-         /* If bytes in buffer is less than max, that means the reader didnt have more
-          * data to read. therefore we can break the loop when this happens. */
-         if(tData->iBytesInBuffer < BUFFER_SIZE){
-            printf("COUNTER: Reader is done reading the file! Completing count ...\n");
-            iReaderComplete = 1;
+            /* Locks mutex while counter is running to prevent unexpected changes */
+            pthread_mutex_lock(&tData->muLock);
+
+            /* If bytes in buffer is less than max, that means the reader didnt have more
+             * data to read. therefore we can break the loop when this happens. */
+            if(tData->iBytesInBuffer < BUFFER_SIZE){
+               printf("COUNTER: Reader is done reading the file! Completing count ...\n");
+               iReaderComplete = 1;
+            }
+
+            /* Counts the number of a byte's occurence in the buffer */
+            /* TODO: Task 2: Replace this with hash and encryption methods */
+            for(i = 0; i < tData->iBytesInBuffer; i++){
+               /* Indexes by the BYTE (value between 0 and MAX_RANGE) and increments that value */
+               iarrByteCount[tData->byarrBuffer[i]]++;
+            }
+
+            /* TODO: DJB2 HASH */
+            
+
+            /* TODO: TEA ENCRYPT */
+
+
+            /* Resets the number of bytes in buffer */
+            tData->iBytesInBuffer = 0;
+
+            /* If bytes were less than max when signal was received, we exit the program here. */
+            if(iReaderComplete == 1){
+               pthread_mutex_unlock(&tData->muLock);
+               break;
+            } else {
+               /* Otherwise we signal back to the reader to continue */
+               printf("COUNTER: Buffer is empty! Signaling reader to start.\n");
+               pthread_mutex_unlock(&tData->muLock);
+               sem_post(&tData->semCounterDone);
+
+            }
+         } /*-> COUNTER ENDLOOP */
+
+         /* Prints counter results to the terminal */
+         for(i = 0; i < BYTE_RANGE; i++){
+            printf("%c: %d - ", i, iarrByteCount[i]);
+            if(i % 5 == 0) puts("");
          }
+      } /*-> COUNTER SETUP WAS SUCCESSFUL ENDIF*/ 
 
-         /* Counts the number of a byte's occurence in the buffer */
-         /* TODO: Task 2: Replace this with hash and encryption methods */
-         for(i = 0; i < tData->iBytesInBuffer; i++){
-            /* Indexes by the BYTE (value between 0 and MAX_RANGE) and increments that value */
-            tData->iarrByteCount[tData->byarrBuffer[i]]++;
-         }
-
-         /* TODO: DJB2 HASH */
-         
-
-         /* TODO: TEA ENCRYPT */
-
-
-         printf("Bytes added to list!\n");
-         /* Resets the number of bytes in buffer */
-         tData->iBytesInBuffer = 0;
-
-         /* If bytes were less than max when signal was received, we exit the program here. */
-         if(iReaderComplete == 1){
-            pthread_mutex_unlock(&tData->muMutex);
-            break;
-         } else {
-            /* Otherwise we signal back to the reader to continue */
-            printf("COUNTER: Buffer is empty! Signaling reader to start.\n");
-            pthread_mutex_unlock(&tData->muMutex);
-            sem_post(&tData->semCounterDone);
-         }
+      /* Closing .hash (if its open) */
+      if(fpHashed != NULL){
+         fclose(fpHashed);
+         fpHashed = NULL;
       }
-      /* Prints counter results to the terminal */
-      for(i = 0; i < BYTE_RANGE; i++){
-         printf("%c: %d - ", i, tData->iarrByteCount[i]);
-         if(i % 5 == 0) puts("");
+
+      /* Same for .enc */
+      if(fpEncrypted != NULL){
+         fclose(fpEncrypted);
+         fpEncrypted = NULL;
       }
-   }
+   } /*-> READER WAS SUCCESSFUL ENDIF */
 
-   /* Closing files (if they are open) */
-   if(fpHashed != NULL){
-      fclose(fpHashed);
-      fpHashed = NULL;
-   }
-
-   if(fpEncrypted != NULL){
-      fclose(fpEncrypted);
-      fpEncrypted = NULL;
-   }
 
    /* Closes thread */
    pthread_exit(NULL);
@@ -293,17 +336,15 @@ int main(int iArgC, char **arrpszArgV) {
    tData->iBytesInBuffer = 0;
    
    /* Set initial boolean flags */
-   tData->iReaderFailed = 0;
-   tData->iCounterFailed = 0;
+   tData->iReaderSetupFailed = -1;
+   tData->iCounterSetupFailed = -1;
 
    /* Initialize mutex (with default attributes) */
-   pthread_mutex_init(&tData->muMutex, NULL);
+   pthread_mutex_init(&tData->muLock, NULL);
 
    /* Since the semaphores only are going to communicate with each other,
     * and there only is two threads, we set their (counter) to 0 */
    sem_init(&tData->semReaderDone, 0, 0);
-
-   /* NOTE: This one is initialized to zero so that reader can start :) */
    sem_init(&tData->semCounterDone, 0, 0);
 
    /* Start threads */
@@ -335,7 +376,7 @@ int main(int iArgC, char **arrpszArgV) {
    }
 
    /* Destroy synchronization elements */
-   pthread_mutex_destroy(&tData->muMutex);
+   pthread_mutex_destroy(&tData->muLock);
    sem_destroy(&tData->semReaderDone); 
    sem_destroy(&tData->semCounterDone); 
 
