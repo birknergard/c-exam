68434b806f9aa9bad64f4786dbf82489b950f9ae
birknergard <birknergard@gmail.com>
2025-05-09 12:48:19 -0400

    Creates header parser which checks content length.

--- pg3401_exam25/task6_87/solution.c
+++ pg3401_exam25/task6_87/solution.c
@@ -17,23 +17,23 @@
 
 int main(int iArgC, char **arrpszArgV){
    /* Checking if arguments were provided */
-   if(iArgC < 4){
-      perror("Insufficient arguments for client start.");
+   if(iArgC < 5){
+      puts("Insufficient arguments for client start.");
       return 1;
    }
 
    /* Parse program arguments 
       If flag was given, store id from arguments */
-   int iPort;
-   if(strcmp(arrpszArgV[1], "-port") == 0){
-      iPort = atoi(arrpszArgV[2]);
-   }
-
    char szServerID[50] = {0};  
-   if(strcmp(arrpszArgV[3], "-id") == 0){
-      memcpy(szServerID, arrpszArgV[4], 49);
+   if(strcmp(arrpszArgV[1], "-id") == 0){
+      memcpy(szServerID, arrpszArgV[2], 49);
    }
    szServerID[49] = '\0';
+
+   int iPort;
+   if(strcmp(arrpszArgV[3], "-port") == 0){
+      iPort = atoi(arrpszArgV[4]);
+   }
 
    /*--------CLIENT SETUP-----------*/
    
@@ -89,23 +89,57 @@ int main(int iArgC, char **arrpszArgV){
    szResponseHeader[MAX_HTTP_HEADER] = '\0';
 
    /* Find start of ContentLength */
-   int iLengthStart = strcspn(szResponseHeader, "\nContent-Length: ");
-   if(iLengthStart == strlen(szResponseHeader)){
-      printf("Invalid header received. Closing ...\n");
-      close(sockClient); 
-      sockClient = -1;
-      return 1;
+   printf("Finding start of ContentLength\n");
+   char szReader[258] = {0};
+   char c;
+   int i, k;
 
+   /* This loop finds the range from the first newline it detects and the following one. */
+   int iarrLine[2], iFoundFirst = -1;
+   memset(iarrLine, -1, sizeof(int) * 2);
+
+   /* I devised this method to "safely" read any http header and find content-length */
+   for(i = 1; i < strlen(szResponseHeader); i++){
+      if(szResponseHeader[i - 1] == '\r' && szResponseHeader[i] == '\n'){
+         iarrLine[0] = i + 1;
+         k = 0;
+         while(k < strlen(szResponseHeader)){
+            szReader[k] = szResponseHeader[iarrLine[0] + k];
+            if(szResponseHeader[iarrLine[0] + k - 1] == '\r' && szResponseHeader[iarrLine[0] + k] == '\n'){
+               iarrLine[1] = k + iarrLine[0] - 2; /* Omitting the \r\n characters */
+               szReader[k] = '\0';
+               printf("\nLINE %s\n", szReader);
+               break;
+            }
+            k++;
+         }
+
+         /* Check line created for match with "Content-Length: " */
+         if(strncmp(szReader, "Content-Length: ", 15) == 0){
+            printf("Content-Length found!\n");
+            
+            /* If found, we store the location of where the number would start " ". 
+             * Where the number will end is already stored in the second index*/
+            iarrLine[0] += 16;
+            break;
+         }
+
+         memset(szReader, 0, 258);
+         i += k;
+      }
    }
 
    /* Finding end index of ContentLength */
-   int iLengthEnd = strcspn(szResponseHeader + iLengthStart, "\n");
+   /*
+   printf("Finding end of ContentLength\n");
+   iLengthEnd += strcspn((szResponseHeader + iLengthStart), "\r\n");
+   */
+   int iDigits = iarrLine[1] - iarrLine[0];
+   int iContentLength = 0, n;
+   char cByte, iByte;
 
-   int iDigits = iLengthEnd - iLengthStart;
-   int iContentLength = -1, i, n;
-   char cByte;
-
-   for(i = iLengthStart; i <= iLengthEnd; i++){
+   /* Calculate size based on every digit in the range */
+   for(i = iarrLine[0]; i <= iarrLine[1]; i++){
       cByte = szResponseHeader[i];
       /* Checks if byte is not digit */
       if(47 > cByte && cByte > 58){
@@ -113,21 +147,34 @@ int main(int iArgC, char **arrpszArgV){
          return 1;
       } 
 
-      int iByte = (int) cByte;
-      /* Multiply itby the power of the 10^digitplace */
-      for(n = 0; n <= iDigits; i++){
+      int iByte = (int) (cByte - 48);
+      /* set digit to the power of the 10^digitplace */
+      for(n = iDigits; 0 < n; n--){
          iByte = iByte * 10;
       }
-      printf("iByte = %d", iByte);
-      iContentLength = iByte;
+      iContentLength += iByte;
 
       iDigits--;
    }
-   printf("ContentLength from header %d", iContentLength);
+
+   if(iContentLength < MAX_HTTP_RESPONSE){
+      printf("Content length of response exceeds max. Exiting ...\n");
+      return 1;
+   }
+
+
+   /*-------------RECIEVING FILE--------------*/
+   /* Receive file */
+   if((iReceived = recv(sockClient, szResponseHeader, MAX_HTTP_HEADER, MSG_PEEK)) < 0){
+      printf("Failed to receive response from server - errcode %d\n", errno);
+      close(sockClient); sockClient = -1;
+      return -2; 
+   } 
+   szResponseHeader[MAX_HTTP_HEADER] = '\0';
+
 
 
    /* Debugging, attempting to print response */
-   printf("RESPONSE=%s\n", szResponseHeader);
    close(sockClient); sockClient = -1;
 
    return 0;
